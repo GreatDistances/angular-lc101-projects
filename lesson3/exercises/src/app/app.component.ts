e731318a9cc0a82eebe7f49dac3e559c3219a45c
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2022-12-08 21:08:52 -0600

    commit

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,63 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeoffEnabled: boolean = true;
+
+  
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+      this.takeoffEnabled = false;
+    }
+  }
+  
+  handleLanding() {
+      let result = window.confirm('Are you ready to land the shuttle?');
+      if (result) {
+        this.color = 'green';
+        this.height = 0;
+        this.width = 0;
+        this.message = 'Space shuttle has landed and is ready for takeoff!';
+      }
+  }
+
+  handleAbort() {
+    let result = window.confirm('Do you want to abort the mission?');
+    if (result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission aborted! Shuttle has landed and is ready for takeoff.';
+    }
+}
+
+moveRocket(rocketImage, direction) {
+   if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.width = this.width + 10000;
+      if (this.height >= 20000) {
+        this.height = this.height - 10000;
+      }
+    }
+  }
 }
